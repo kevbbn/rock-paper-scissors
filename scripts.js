526b15c86309c6d80618b758640b5ec17d2b2a50
kevin <k_pham@ucsb.edu>
2021-02-24 15:29:53 -0800

    Finished

--- scripts.js
+++ scripts.js
@@ -11,22 +11,90 @@ function computerPlay(){ // randomly returns rock paper or scissors
     }
 
   }
+
+function clear(){
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach(choice => {
+        choice.classList.remove("win");
+        choice.classList.remove("lose");
+        choice.classList.remove("tie");
+    });
+}
+function tie(playerSelection){
+    clear();
+    let player = document.getElementById(`${playerSelection}`);
+    let computer = document.getElementById(`computer-${playerSelection}`);
+    player.classList.add("tie");
+    computer.classList.add("tie");
+}
+function win(playerSelection){
+    clear();
+    let player = document.getElementById(`${playerSelection}`);
+    if(playerSelection=="rock"){
+        let computer = document.getElementById("computer-scissors");
+        player.classList.add("win");
+        computer.classList.add("lose");
+
+    }
+    else if(playerSelection=="paper"){
+        let computer = document.getElementById("computer-rock");
+        player.classList.add("win");
+        computer.classList.add("lose");
+    }
+    else{ // player = scissors, comuter == paper
+        let computer = document.getElementById("computer-paper");
+        player.classList.add("win");
+        computer.classList.add("lose");
+    }
+}
+function lose(playerSelection){
+    clear();
+    let player = document.getElementById(`${playerSelection}`);
+    if(playerSelection=="rock"){
+        let computer = document.getElementById("computer-paper");
+        player.classList.add("lose");
+        computer.classList.add("win");
+
+    }
+    else if(playerSelection=="paper"){
+        let computer = document.getElementById("computer-scissors");
+        player.classList.add("lose");
+        computer.classList.add("win");
+    }
+    else{ // player = scissors, comuter == paper
+        let computer = document.getElementById("computer-rock");
+        player.classList.add("lose");
+        computer.classList.add("win");
+    }
+
+}
 function playRound(e) {
     let computerSelection = computerPlay(); 
     let playerSelection = e.toElement.id;
+    if(playerSelection == "rock-img"){ // if user clicks the img instead of the div
+        playerSelection = "rock";
+    }
+    else if(playerSelection == "paper-img"){
+        playerSelection = "paper";
+    }
+    else if(playerSelection == "scissors-img"){
+        playerSelection = "scissors";
+    }
     let result = ""
     if(playerSelection==computerSelection){
         result = "Tie! No one wins!"
+        tie(playerSelection);
     }
     else if(playerSelection=="rock"){
         if(computerSelection=="scissors"){ // rock wins (player)
             result = "You win! Rock beats scissors!";
-
             playerScore++;
+            win(playerSelection);
         }
         else{ // paper wins (computer)
             result = "You lose! Paper beats rock!";
             computerScore++;
+            lose(playerSelection);
         }
 
     }
@@ -34,20 +102,24 @@ function playRound(e) {
         if(computerSelection=="scissors"){ // scissor wins (computer)
             result = "You lose! Scissors beats paper!";
             computerScore++;
+            lose(playerSelection);
         }
         else{ // paper wins (player)
             result = "You win! Paper beats rock";
             playerScore++;
+            win(playerSelection);
         }
     }
     else if(playerSelection=="scissors"){ // player selection == scissors
         if(computerSelection=="rock"){ // rock wins (computer)
             result = "You lose! Rock beats scissors!";
             computerScore++;
+            lose(playerSelection);
         }
         else{ // scissors wins (player)
             result = "You win! Scissors beats paper!";
             playerScore++;
+            win(playerSelection);
         }
     }
     else{
